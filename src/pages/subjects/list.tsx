8f7461523df322f906a3baa80b67f4410694743a
Juha Duong <juha.duong@edu.turkuamk.fi>
2026-02-06 13:32:26 +0200

    Saving changes for the lists in the subjects view. In the middle of the tutorial left in timestamp: 2:33:15.

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,7 +1,105 @@
-import React from "react";
+import { CreateButton } from "@/components/refine-ui/buttons/create";
+import { DataTable } from "@/components/refine-ui/data-table/data-table";
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { DEPARTMENT_OPTIONS } from "@/constants";
+import { Subject } from "@/types";
+import { useTable } from "@refinedev/react-table";
+import { Search } from "lucide-react";
+import React, { useMemo } from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import { Badge } from "@/components/ui/badge";
 
 function SubjectsList() {
-  return <div>list</div>;
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const [selectDepartment, setSelectDepartment] = React.useState("all");
+  const subjectTable = useTable<Subject>({
+    columns: useMemo<ColumnDef<Subject>[]>(
+      () => [
+        {
+          id: "id",
+          accessorKey: "code",
+          size: 100,
+          header: () => <p className="column-title ml-2">Code</p>,
+          cell: ({ getValue }) => <Badge>{getValue<string>()}</Badge>,
+        },
+        {
+          id: "name",
+          accessorKey: "name",
+          size: 200,
+          header: () => <p className="column-title">Name</p>,
+          cell: ({ getValue }) => (
+            <span className="text-foreground">{getValue<string>()}</span>
+          ),
+        },
+      ],
+      [],
+    ),
+    refineCoreProps: {
+      resource: "subjects",
+      pagination: { pageSize: 10, mode: "server" },
+      filters: {},
+      sorters: {},
+    },
+  });
+
+  return (
+    <ListView>
+      <Breadcrumb />
+
+      <h1 className="page-title">Subjects</h1>
+
+      <div className="intro-row">
+        <p>Quick access to essential metrics and management tools.</p>
+
+        <div className="actions-row">
+          <div className="search-field">
+            <Search className="search-icon" />
+
+            <Input
+              type="text"
+              placeholder="Search by name ..."
+              className="pl-10 w-full"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+
+          <div className="flex gap-2 w-full sm:w-auto">
+            <Select
+              value={selectDepartment}
+              onValueChange={setSelectDepartment}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Filter by department" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectItem value="all">All Departments</SelectItem>
+                {DEPARTMENT_OPTIONS.map((department) => (
+                  <SelectItem key={department.value} value={department.value}>
+                    {department.lable}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+
+            <CreateButton />
+          </div>
+        </div>
+      </div>
+
+      <DataTable table={subjectTable} />
+    </ListView>
+  );
 }
 
 export default SubjectsList;
